46372f3deeeaa7caae83e24ab10802b5210d5d5c
andrewelamb <andrewelamb@gmail.com>
2021-02-08 09:42:31 -0800

    add filter argument

--- R/synapse_functions.R
+++ R/synapse_functions.R
@@ -11,9 +11,12 @@ create_synapse_login <- function(){
 #' @param syn An object of reticulate::import("synapseclient")$Synapse()
 #' @param table_id A synapse table id
 #' @param columns A vector of columns to get from the syanpse table
+#' @param filters A vector of strings. Filter to add onto the end of the query
 #' @param limit An integer, limits the number of rows in the table
 #' @export
-get_synapse_tbl <- function(syn, table_id, columns = NULL, limit = NULL){
+get_synapse_tbl <- function(
+  syn, table_id, columns = NULL, filters = NULL, limit = NULL
+){
   list_columns <- table_id %>%
     syn$getTableColumns() %>%
     reticulate::iterate(.) %>%
@@ -32,16 +35,26 @@ get_synapse_tbl <- function(syn, table_id, columns = NULL, limit = NULL){
     list_columns <- purrr::keep(list_columns, list_columns %in% columns)
   }
 
-  query_string <- glue::glue("SELECT {column_string} FROM {table_id}")
+  query_string <- "SELECT {column_string} FROM {table_id}"
 
-  if(is.null(limit)){
-    query_string <- glue::glue("SELECT {column_string} FROM {table_id}")
-  } else {
-    query_string <- glue::glue(
-      "SELECT {column_string} FROM {table_id} limit {limit}"
-    )
+  if(!is.null(filters)){
+    query_string <-
+      stringr::str_c(
+        query_string,
+        "WHERE",
+        stringr::str_c(filters, collapse = " AND "),
+        sep = " "
+      )
   }
+
+  if(!is.null(limit)){
+    query_string <- stringr::str_c(query_string, " LIMIT {limit}")
+  }
+
   query_string %>%
+    print() %>%
+    glue::glue() %>%
+    print() %>%
     syn$tableQuery() %>%
     purrr::pluck("filepath") %>%
     readr::read_csv(.) %>%

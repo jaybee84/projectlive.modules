ef26cd50092661b7511aa53b4293cb83d7d39f5a
Andrew Lamb <andrewelamb@gmail.com>
2021-02-20 10:07:07 -0800

    uncommit tests

--- tests/testthat/test-study_summary_module.R
+++ tests/testthat/test-study_summary_module.R
@@ -1,28 +1,28 @@
-#
-# test_that("study_summary_module_ui", {
-#   expect_type(study_summary_module_ui("id"), "list")
-# })
-#
-# test_that("nf_study_summary_module_server", {
-#   shiny::testServer(
-#     study_summary_module_server,
-#     args = list(
-#       "data" = shiny::reactiveVal(nf_data),
-#       "config" = shiny::reactiveVal(nf_study_summary_config)
-#     ),
-#     {
-#       expect_type(output$header_text, "character")
-#       session$setInputs("merge_studies-study_table_rows_selected" = 3)
-#
-#       expect_type(output$study_summary, "character")
-#       expect_type(output$study_timeline_plot, "character")
-#       expect_type(output$data_focus_plot, "character")
-#       expect_type(output$annotation_activity_plot, "character")
-#       expect_type(output$publication_status_plot, "character")
-#
-#     }
-#   )
-# })
+
+test_that("study_summary_module_ui", {
+  expect_type(study_summary_module_ui("id"), "list")
+})
+
+test_that("nf_study_summary_module_server", {
+  shiny::testServer(
+    study_summary_module_server,
+    args = list(
+      "data" = shiny::reactiveVal(nf_data),
+      "config" = shiny::reactiveVal(nf_study_summary_config)
+    ),
+    {
+      expect_type(output$header_text, "character")
+      session$setInputs("merge_studies-study_table_rows_selected" = 3)
+
+      expect_type(output$study_summary, "character")
+      expect_type(output$study_timeline_plot, "character")
+      expect_type(output$data_focus_plot, "character")
+      expect_type(output$annotation_activity_plot, "character")
+      expect_type(output$publication_status_plot, "character")
+
+    }
+  )
+})
 
 test_that("nf_gff_study_summary_module_server", {
   shiny::testServer(
@@ -56,28 +56,28 @@ test_that("nf_gff_study_summary_module_server", {
   )
 })
 
-#
-# test_that("csbc_study_summary_module_server", {
-#   shiny::testServer(
-#     study_summary_module_server,
-#     args = list(
-#       "data" = shiny::reactiveVal(csbc_data),
-#       "config" = shiny::reactiveVal(csbc_study_summary_config)
-#     ),
-#     {
-#       expect_type(output$header_text, "character")
-#       session$setInputs("merge_studies-study_table_rows_selected" = 34)
-#       expect_equal(
-#         data2()$selected_study,
-#         "H Lee Moffitt Cancer Center and Research Institute"
-#       )
-#
-#       expect_type(output$study_summary, "character")
-#       expect_type(output$study_timeline_plot, "character")
-#       expect_type(output$data_focus_plot, "character")
-#       expect_type(output$annotation_activity_plot, "character")
-#       expect_type(output$publication_status_plot, "character")
-#
-#     }
-#   )
-# })
+
+test_that("csbc_study_summary_module_server", {
+  shiny::testServer(
+    study_summary_module_server,
+    args = list(
+      "data" = shiny::reactiveVal(csbc_data),
+      "config" = shiny::reactiveVal(csbc_study_summary_config)
+    ),
+    {
+      expect_type(output$header_text, "character")
+      session$setInputs("merge_studies-study_table_rows_selected" = 34)
+      expect_equal(
+        data2()$selected_study,
+        "H Lee Moffitt Cancer Center and Research Institute"
+      )
+
+      expect_type(output$study_summary, "character")
+      expect_type(output$study_timeline_plot, "character")
+      expect_type(output$data_focus_plot, "character")
+      expect_type(output$annotation_activity_plot, "character")
+      expect_type(output$publication_status_plot, "character")
+
+    }
+  )
+})

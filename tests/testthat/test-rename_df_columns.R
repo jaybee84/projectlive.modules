1d3ee6b9ff01203aa1c8adb2de93f751dd24581f
Andrew Lamb <andrewelamb@gmail.com>
2021-07-27 08:59:03 -0700

    fix broken test

--- tests/testthat/test-rename_df_columns.R
+++ tests/testthat/test-rename_df_columns.R
@@ -1,27 +1,35 @@
 test_that("rename_df_columns_with_config", {
-  tbl1 <- dplyr::tibble("col1" = c(), "col2" = c(), "colNum3" = c())
+  tbl1 <- dplyr::tibble(
+    "col1" = character(), "col2" = character(), "colNum3" = character()
+  )
   config1 <- list(
     "columns" = list(
-      "col1" = list("name" = "col1", "display_name" = "Column1", "type" = "x"),
-      "col2" = list("name" = "col2", "type" = "x"),
-      "col3" = list("name" = "colNum3", "type" = "x")
-    )
-  )
-  config2 <- list(
-    "columns" = list(
-      list("name" = "col1", "display_name" = "Column1", "type" = "x"),
-      list("name" = "col2", "type" = "x", "display_name" = "Column2"),
-      list("name" = "colNum3", "type" = "x", "display_name" = "Column3")
+      "col1" = list("name" = "col1", "display_name" = "Column1"),
+      "col2" = list("name" = "col2"),
+      "col3" = list("name" = "colNum3")
     )
   )
   res1 <- rename_df_columns_with_config(tbl1, config1)
-  res2 <- rename_df_columns_with_config(tbl1, config2)
   expect_equal(
     res1,
-    dplyr::tibble("Column1" = c(), "Col2" = c(), "Colnum3" = c())
+    dplyr::tibble(
+      "Column1" = character(), "Col2" = character(), "Colnum3" = character()
+    )
   )
+
+
+  config2 <- list(
+    "columns" = list(
+      list("name" = "col1", "display_name" = "Column1"),
+      list("name" = "col2", "display_name" = "Column2"),
+      list("name" = "colNum3", "display_name" = "Column3")
+    )
+  )
+  res2 <- rename_df_columns_with_config(tbl1, config2)
   expect_equal(
     res2,
-    dplyr::tibble("Column1" = c(), "Column2" = c(), "Column3" = c())
+    dplyr::tibble(
+      "Column1" = character(), "Column2" = character(), "Column3" = character()
+    )
   )
 })
